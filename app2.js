40fddcb245d6a727a61fd633f3813d3b909b61a2
gasparpeter <petihappygaspar@gmail.com>
2019-09-09 20:59:56 +0300

    - created the list place

--- app2.js
+++ app2.js
@@ -1,7 +1,9 @@
 let gomb = document.getElementById('gomb');
 let doboz = document.getElementById('doboz');
+let gomb2 = document.getElementById('gomb2');
 
 gomb.addEventListener("click", function () {
    doboz.style.display = 'block';
    gomb.style.display = 'none';
 });
+
