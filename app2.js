37474761561de91607cc82581c992402ad864539
gasparpeter <petihappygaspar@gmail.com>
2019-09-09 21:30:32 +0300

    - rendering value input on list

--- app2.js
+++ app2.js
@@ -1,9 +1,17 @@
 let gomb = document.getElementById('gomb');
 let doboz = document.getElementById('doboz');
 let gomb2 = document.getElementById('gomb2');
+let mailInp = document.getElementById('mailInp');
+let titleInp = document.getElementById('titleInp');
+let area = document.getElementById('area');
+let list = document.getElementById('list');
 
 gomb.addEventListener("click", function () {
    doboz.style.display = 'block';
    gomb.style.display = 'none';
 });
 
+
+   gomb2.addEventListener('click', function () {
+         list.innerText = titleInp.value
+   });
