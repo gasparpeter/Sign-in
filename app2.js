541ff35330aa37dfdb62e8a70da550b924393434
gasparpeter <petihappygaspar@gmail.com>
2019-09-13 15:49:40 +0300

    - created objects, appendchild title to list

--- app2.js
+++ app2.js
@@ -7,63 +7,37 @@ let area = document.getElementById('area');
 let list = document.getElementById('list');
 let cancel = document.getElementById('cancel');
 let eror = document.getElementById('eror');
-let infobox = document.getElementById('infobox');
 let X = document.getElementById('X');
 
+let EMAILS = [];
+
 gomb.addEventListener("click", function () {
-   doboz.style.display = 'block';
-   gomb.style.display = 'none';
+    doboz.style.display = 'block';
+    gomb.style.display = 'none';
 });
 
 cancel.addEventListener('click', function () {
-   doboz.style.display = 'none';
-   gomb.style.display = 'block';
+    doboz.style.display = 'none';
+    gomb.style.display = 'block';
+});
+
+
+gomb2.addEventListener('click', function () {
+   EMAILS.push({
+       "email": mailInp.value,
+       "title": titleInp.value,
+       "message": area.value
+   });
+
+
+    const emailTitle = document.createElement( "li" );
+    emailTitle.innerText = titleInp.value;
+
+    list.appendChild(emailTitle);
 });
 
 
 
 
-   gomb2.addEventListener('click', function () {
 
 
-       if (titleInp.value.length > 3 || mailInp.value.length > 3 || area.value.length > 3) {
-           eror.style.display = 'none';
-       }
-
-       if (titleInp.value.length < 3 || mailInp.value.length < 3 || area.value.length < 3) {
-           eror.style.display = 'block';
-
-       }else {
-           let title = document.createElement("li");
-           title.innerText = titleInp.value;
-           list.appendChild(title);
-           // mailInp.value = null;
-           // titleInp.value = null;
-           // area.value = null;
-           // list.innerText = titleInp.value
-
-            let email = document.createElement("h1");
-            let textarea = document.createElement('p');
-            email.innerText = mailInp.value;
-            textarea.innerText = area.value;
-            infobox.appendChild(email);
-            infobox.appendChild(textarea);
-            infobox.style.display = 'none';
-
-           title.addEventListener('click', function () {
-              infobox.style.display = 'block';
-               mailInp.value = null;
-               titleInp.value = null;
-               area.value = null;
-           });
-
-           X.addEventListener('click', function () {
-               infobox.style.display = 'none';
-           })
-
-       }
-
-
-
-
-   });
